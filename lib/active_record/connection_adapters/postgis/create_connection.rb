1dd92544527231eb321beb993558b09778ed60fc
Ilya Lavrov <dzjuck@gmail.com>
2016-02-14 21:40:30 +0800

    Add support for activerecord 5.0.0.beta

--- lib/active_record/connection_adapters/postgis/create_connection.rb
+++ lib/active_record/connection_adapters/postgis/create_connection.rb
@@ -19,6 +19,23 @@ module ActiveRecord  # :nodoc:
 
       # Based on the default <tt>postgresql_connection</tt> definition from ActiveRecord.
       # https://github.com/rails/rails/blob/master/activerecord/lib/active_record/connection_adapters/postgresql_adapter.rb
+      # def postgis_connection(config)
+      #   # FULL REPLACEMENT because we need to create a different class.
+      #   conn_params = config.symbolize_keys
+
+      #   conn_params.delete_if { |_, v| v.nil? }
+
+      #   # Map ActiveRecords param names to PGs.
+      #   conn_params[:user] = conn_params.delete(:username) if conn_params[:username]
+      #   conn_params[:dbname] = conn_params.delete(:database) if conn_params[:database]
+
+      #   # Forward only valid config params to PGconn.connect.
+      #   conn_params.keep_if { |k, _| VALID_CONN_PARAMS.include?(k) }
+
+      #   # The postgres drivers don't allow the creation of an unconnected PGconn object,
+      #   # so just pass a nil connection object for the time being.
+      #   ConnectionAdapters::PostGISAdapter.new(nil, logger, conn_params, config)
+      # end
       def postgis_connection(config)
         # FULL REPLACEMENT because we need to create a different class.
         conn_params = config.symbolize_keys
@@ -30,7 +47,8 @@ module ActiveRecord  # :nodoc:
         conn_params[:dbname] = conn_params.delete(:database) if conn_params[:database]
 
         # Forward only valid config params to PGconn.connect.
-        conn_params.keep_if { |k, _| VALID_CONN_PARAMS.include?(k) }
+        valid_conn_param_keys = PGconn.conndefaults_hash.keys + [:requiressl]
+        conn_params.slice!(*valid_conn_param_keys)
 
         # The postgres drivers don't allow the creation of an unconnected PGconn object,
         # so just pass a nil connection object for the time being.
